160984a0a6d921ada0648125d74c50cf512e74dd
Anna Aleshko <aleshkoa@tcd.ie>
2019-07-04 11:12:24 +0100

    Create question5.py

---
+++ question5.py
@@ -0,0 +1,24 @@
+try:
+score = float(raw_input('Please enter a score between 0.0 and 10.0: '))
+except:
+print('Programme hit an error. Please try again. Make you you enter a numerical va
+lue')
+exit()
+
+if score > 10.0:
+print('Score too high, please enter score between 0.0. and 10.0')
+
+elif score>=9.0 :
+print('Grade A')
+
+elif score>=8.0 :
+print 'Grade B'
+
+elif score>=7.0 :
+print 'Grade C'
+
+elif score>=6.0 :
+print 'Grade D'
+
+else:
+print'F'
