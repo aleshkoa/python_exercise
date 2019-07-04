f3a3624963ee44918e747a18aff31fe1324fcc01
Anna Aleshko <aleshkoa@tcd.ie>
2019-07-04 11:12:03 +0100

    Create question11.py

---
+++ question11.py
@@ -0,0 +1,26 @@
+filename = raw_input('Enter file name: ')
+
+try:
+fname = open(filename, "r").read().split()
+except:
+print'Invalid filename'
+exit()
+
+cleanstring = [''.join(e for e in string if e.isalnum()) for string in fname]
+
+clstring = ''.join(cleanstring)
+
+catstring = clstring.lower()
+
+freq={}
+
+for letter in catstring:
+if letter in freq:
+freq[letter] +=1
+else:
+freq[letter] = 1
+
+items = [(v,k) for k, v in freq.items()]
+items.sort()
+items.reverse()
+print items
