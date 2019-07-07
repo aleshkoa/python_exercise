8a00cb6399fadc2e2539f0032eb0db87d597b793
Anna Aleshko <aleshkoa@tcd.ie>
2019-07-07 19:18:53 +0100

    Update question9.py

--- question9.py
+++ question9.py
@@ -1,16 +1,16 @@
-fname = raw_input("Enter file name: ")
+fname = input("Enter file name: ")
 
 if fname == "money makes the world go round":
-print'mo money mo problems'
-exit()
+    print('mo money mo problems')
+    exit()
 else:
 
-try:
-filename = open(fname)
+    try:
+        filename = open(fname)
 
-for i in filename:
-i = i.rstrip().upper()
-print i
-except:
-print 'Invalid file name.'
-exit()
+        for i in filename:
+            i = i.rstrip().upper()
+            print(i)
+    except:
+        print('Invalid file name.')
+        exit()
