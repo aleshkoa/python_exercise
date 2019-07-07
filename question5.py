4a8d60ee95feb966db194437285044f87ebea5a2
Anna Aleshko <aleshkoa@tcd.ie>
2019-07-07 18:55:34 +0100

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,24 +1,23 @@
 try:
-score = float(raw_input('Please enter a score between 0.0 and 10.0: '))
+    score = float(input('Please enter a score between 0.0 and 10.0: '))
 except:
-print('Programme hit an error. Please try again. Make you you enter a numerical va
-lue')
-exit()
+    print('Programme hit an error. Please try again. Make you you enter a numerical value')
+    exit()
 
 if score > 10.0:
-print('Score too high, please enter score between 0.0. and 10.0')
+    print('Score too high, please enter score between 0.0. and 10.0')
 
 elif score>=9.0 :
-print('Grade A')
+    print('Grade A')
 
 elif score>=8.0 :
-print 'Grade B'
+    print('Grade B')
 
 elif score>=7.0 :
-print 'Grade C'
+    print('Grade C')
 
-elif score>=6.0 :
-print 'Grade D'
+elif score>=6.0:
+    print('Grade D')
 
 else:
-print'F'
+    print('Grade F')
